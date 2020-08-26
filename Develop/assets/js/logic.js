abeb09e5f0b5bd94b6d97586b66626bb41e5676c
Sean Kirkpatrick <Skirkp18@gmail.com>
2020-08-26 14:42:57 -0400

    added to the questionClick function. Added sound effects for when right or wrong answer is clicked

--- Develop/assets/js/logic.js
+++ Develop/assets/js/logic.js
@@ -19,80 +19,97 @@ var sfxWrong = new Audio("assets/sfx/incorrect.wav");
 function startQuiz() {
   // hide start screen
   document.querySelector("#start-screen").setAttribute("class", "hide");
- 
+
   // un-hide questions section
   questionsEl.setAttribute("class", "");
 
   // start timer
-  setInterval(function() {
-  timerId++;
+  setInterval(function () {
+    timerId++;
     // show starting time
-  timerEl.textContent = timerId;
+    timerEl.textContent = timerId;
 
-  console.log(timerId);
+    console.log(timerId);
 
   }, 1000);
 
-  
+
 
   getQuestion();
 }
 
 function getQuestion() {
   // get current question object from array
-  var currentQuestion = questions[currentQuestionIndex];
-  
+   currentQuestion = questions[currentQuestionIndex];
+
   // update title with current question
   document.getElementById("question-title").innerHTML = currentQuestion["title"];
-  
+
   // console.log(currentQuestion)
   console.log(currentQuestion);
-  
+
   // clear out any old question choices
   document.getElementById("choices").innerHTML = "";
 
   // loop over choices
   for (var i = 0; i < currentQuestion.choices.length; i++) {
 
- // create new button for each choice
-  var button = document.createElement("button");
-  
-  // attach click event listener to each choice
+    // create new button for each choice
+    var button = document.createElement("button");
 
-  button.addEventListener("click", questionClick());
+    // attach click event listener to each choice
 
-  // display on the page
+    button.addEventListener("click", function(event) {
+      element = event.target.innerHTML;
+      questionClick();
+    });
 
-  button.textContent = currentQuestion.choices[i];
+    // display on the page
 
-  choicesEl.appendChild(button);
-}
+    button.textContent = currentQuestion.choices[i];
+
+    choicesEl.appendChild(button);
+  }
 }
 
 function questionClick(event) {
-  // event.preventDefault();
+
+  console.log(element);
+  console.log(currentQuestion.answer);
 
   // check if user guessed wrong
-  // if (event.target.matches(cur)
+  if (element !== currentQuestion.answer) {
+    
+  console.log("wrong");
     // penalize time
 
     // display new time on page
 
     // play "wrong" sound effect
-
+    sfxWrong.play();
+  
+  }
   // else 
+  else {
+    console.log("right");
+
     // play "right" sound effect
 
+    sfxRight.play();
+  }
 
-  // flash right/wrong feedback on page for half a second
 
-  // move to next question
+    // flash right/wrong feedback on page for half a second
 
-  // check if we've run out of questions
+    // move to next question
+
+    // check if we've run out of questions
     // quizEnd
-  // else 
+    // else 
     // getQuestion
+  
 }
+
 
 function quizEnd() {
   // stop timer
@@ -114,18 +131,18 @@ function saveHighscore() {
   // get value of input box
 
   // make sure value wasn't empty
-    // get saved scores from localstorage, or if not any, set to empty array
+  // get saved scores from localstorage, or if not any, set to empty array
 
-    // format new score object for current user
+  // format new score object for current user
 
-    // save to localstorage
+  // save to localstorage
 
-    // redirect to next page
+  // redirect to next page
 }
 
 function checkForEnter(event) {
   // check if event key is enter
-    // saveHighscore
+  // saveHighscore
 }
 
 // user clicks button to submit initials
