bc17eb17672096948ebc2379dd940778cb8e23e2
Sean Kirkpatrick <Skirkp18@gmail.com>
2020-08-29 15:04:04 -0400

    fixed local storage issues

--- Develop/assets/js/scores.js
+++ Develop/assets/js/scores.js
@@ -1,23 +1,36 @@
 var clearBtn = document.getElementById("clear");
-
-
-var highScoreEl = document.getElementById("savedScores");
+var highScoreEl = document.getElementById("highscores");
 
 function printHighscores() {
   // either get scores from localstorage or set to empty array
- 
-  // savedHighScores = JSON.parse(savedHighScores);
-  
+var highScores = localStorage.getItem("savedScores");
+if (highScores !== null) {
+var parsedHighScores = JSON.parse(highScores);
+console.log(parsedHighScores);
+} else {
+  var highScores = [];
+  console.log("no scores yet");
+}
 
+ 
   // (optional) sort highscores by score property in descending order
 
+  console.log(parsedHighScores.length);
+
+  // highScoreEl.innerHTML = ""
+
   // for each score
-
+  for (var i = 0; i < parsedHighScores.length; i++){
+    console.log("here");
+  
     // create li tag for each high score
-
-
+    var li = document.createElement("li");
+    highScoreEl.append(parsedHighScores);
 
     // display on page
+    highScoreEl.append(li)
+  }
+  
 }
 
 function clearHighscores() {
@@ -25,7 +38,7 @@ function clearHighscores() {
 }
 
 // attache clear event to clear score button
-clearBtn.onclick = localStorage.clear();
+// clearBtn.onclick = localStorage.clear();
 
 // run printhighscore when page loads
 printHighscores();
